b71bb2def27e342543c5e078cbfa353bb6302dcd
Greg Palmier <greg.palmier@tempus.com>
2020-04-05 17:56:19 -0500

    add some basic tests

--- spec/pastebin_spec.rb
+++ spec/pastebin_spec.rb
@@ -1,5 +1,14 @@
 RSpec.describe Pastebin do
+  let(:pastebin) { Pastebin::API.new }
   it "has a version number" do
     expect(Pastebin::VERSION).not_to be nil
+  end
+
+  it "Get list of recent pastes" do
+    expect(pastebin.recent).not_to be nil
+  end
+
+  it "Get a specific paste" do
+    expect(pastebin.get_paste('4crx1G5N')).not_to be nil
   end
 end
